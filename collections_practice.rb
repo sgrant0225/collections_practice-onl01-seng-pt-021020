7bbb3b19122bb4f60494aec9dfb2e4f3dab3f49a
Sharnell Grant <sharnellgrant@gmail.com>
2020-02-19 00:10:09 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,36 @@
+require 'pry'
+
+def sort_array_asc(array_of_integers)
+ array_of_integers.sort
+end  
+
+def sort_array_desc(array)
+  array.sort do |a, b|
+  b <=> a
+ end
+end  
+
+def sort_array_char_count(array_strings)
+ array_strings.sort do |a, b|
+  a.length <=> b.length
+ end
+end
+
+def swap_elements(array)
+ array.sort do |a, b|
+  a[2] <=> b[1]
+ end
+end
+
+def reverse_array(array_integers)
+ array_integers.reverse
+end
+
+def kesha_maker(array_of_strings)
+  #new_string = array_of_strings.join(" , ")
+  array_of_strings.each do |string|
+    binding.pry
+    #string[2].sub!("a", "$")
+    end 
+end 
 
