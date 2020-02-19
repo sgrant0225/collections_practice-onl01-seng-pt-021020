df1ecfb2cafaa4ff8012cbf6dda11bbd1dd71348
Sharnell Grant <sharnellgrant@gmail.com>
2020-02-19 03:19:33 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 def sort_array_asc(array_of_integers)
  array_of_integers.sort
 end  
@@ -27,10 +25,29 @@ def reverse_array(array_integers)
 end
 
 def kesha_maker(array_of_strings)
-  #new_string = array_of_strings.join(" , ")
   array_of_strings.each do |string|
-    binding.pry
-    #string[2].sub!("a", "$")
-    end 
-end 
+  string[2] = ("$")
+ end 
+end
 
+def find_a(array_of_fruits)
+  array_of_fruits.select do |letter|
+    letter.start_with?("a")
+  end  
+end
+
+def sum_array(num)
+ num.inject do |sum, n|
+   sum + n
+ end
+end
+
+def add_s(array)
+  array.each_with_index.collect do |element, index|
+  if element == "feet" 
+    "feet"
+  else
+    element + "s"
+  end
+ end
+end  
